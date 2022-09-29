54b11d2338d7cc65549132d44c492945fcb33687
Boris Dryabin <b.dryabin@rt-solar.ru>
2022-09-29 13:26:21 +0300

    first commit

---
+++ src/components/Feeds.vue
@@ -0,0 +1,23 @@
+<template>
+  <Header>
+    <div class="header__logo">
+      <p>Logo</p>
+    </div>
+    <div class="header__buttons">
+      <p>buttons</p>
+    </div>
+  </Header>
+  <Slider>
+    
+  </Slider>
+</template>
+
+<script>
+import Header from "@/components/Header";
+import Slider from "@/components/Slider";
+
+export default {
+  name: "feeds",
+  components: { Header, Slider },
+};
+</script>
---
+++ src/components/Slider.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="slider">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Slider",
+};
+</script>
+
+<style scoped>
+.slider {
+  display: flex;
+  align-items: center;
+  justify-content: flex-start;
+}
+</style>
